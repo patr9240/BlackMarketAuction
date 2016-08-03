d0ad7ec92fbf5508ee7510820fa54e265b2009ef
patr9240 <patr9240@gmail.com>
2016-08-03 19:26:39 -0400

    Add project files.

---
+++ BlackMarketAuction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlackMarketAuction.Startup))]
+namespace BlackMarketAuction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
